fe6487546d786026238fe7577ae717deeccdc35f
Inom T. <i@inomoz.ru>
2025-11-26 22:33:49 +0300

    Initialize tests workflow, refactoring backend project files

--- backend/tests/integration/test_chat.py
+++ backend/tests/integration/test_chat.py
@@ -2,4 +2,6 @@ from app import ChatDownloader
 
 def test_download():
     chat_downloader = ChatDownloader()
-    chat = chat_downloader.download("2626748422")
+    # TODO: hardcoded value
+    chat_log = chat_downloader.download("2626748422")
+    assert len(chat_log) > 0
