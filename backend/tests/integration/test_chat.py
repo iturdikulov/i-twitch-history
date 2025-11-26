e37a3c977b2f30f0b0171235ac159570941dc8b2
Inom T. <i@inomoz.ru>
2025-11-26 19:55:54 +0300

    Refactoring backend file/dir. names, to follow new project structure

--- backend/tests/integration/test_chat.py
+++ backend/tests/integration/test_chat.py
@@ -1,8 +1,5 @@
-from os import environ
-
-from twichist import ChatDownloader
+from app import ChatDownloader
 
 def test_download():
     chat_downloader = ChatDownloader()
     chat = chat_downloader.download("2626748422")
-    print(chat)
